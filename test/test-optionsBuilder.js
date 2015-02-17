a20751e6f8e62e75a703f3a5fbab4bb22d0d2573
Duncan Hall <himself@duncanhall.net>
2015-02-17 17:51:57 +0000

    Test options builder

--- test/test-optionsBuilder.js
+++ test/test-optionsBuilder.js
@@ -29,3 +29,35 @@ test('optionsBuilder swaggerizeOptions', function (t) {
     });
 
 });
+
+test('optionsBuilder expressOptions', function (t) {
+
+    t.test('prefix base path', function (t) {
+        t.plan(1);
+
+        var basePath = 'example_path';
+        var options = {
+            api: {
+                basePath: basePath
+            }
+        };
+        options = optionsBuilder.expressOptions(options);
+        t.equal(options.api.basePath, '/' + basePath);
+    });
+
+    t.test('use routePath', function (t) {
+        t.plan(1);
+
+        var basePath = '/base_path';
+        var routePath = '/route_path';
+        var options = {
+            api: {
+                basePath: basePath
+            },
+            routePath: routePath
+        };
+        options = optionsBuilder.expressOptions(options);
+        t.equal(options.api.basePath, routePath + basePath);
+    });
+
+});
